885f03e64ad45d03c85f68391cb9840fb0aeb3d6
Sairikei <gedionsaiyuah@gmail.com>
2022-06-06 13:55:04 -0700

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -11,27 +11,27 @@ counter = 0
 
 try:
     for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
+    line_list = line.split(" ")
+    if len(line_list) > 4:
+        code = line_list[-2]
+        size = int(line_list[-1])
             if code in cache.keys():
                 cache[code] += 1
             total_size += size
             counter += 1
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+    if counter == 10:
+        counter = 0
+        print('File size: {}'.format(total_size))
+        for key, value in sorted(cache.items()):
+            if value != 0:
+                print('{}: {}'.format(key, value))
 
 except Exception as err:
     pass
 
 finally:
     print('File size: {}'.format(total_size))
-     for key, value in sorted(cache.items()):
-         if value != 0:
-             print('{}: {}'.format(key, value))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
